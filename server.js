c35d35ffbd9007140c0983ed87dcb5d2b1779e64
MIKOU Mouad <mikou.mouad.mail@gmail.com>
2022-07-11 14:09:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 ﻿var express = require('express');
 var app = express();
-var port = process.env.port || 1337
+var port = process.env.port || 8080
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
